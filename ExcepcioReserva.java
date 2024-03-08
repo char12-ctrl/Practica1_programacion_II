ad4d3abe868f27eb1e664284e9483ed0cf52215f
char12-ctrl <75255074+char12-ctrl@users.noreply.github.com>
2024-03-08 13:46:56 +0300

    Create ExcepcioReserva.java

---
+++ ExcepcioReserva.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package prog2.vista;
+
+/**
+ *
+ * @author nacho
+ */
+public class ExcepcioReserva extends Exception{
+
+    public ExcepcioReserva(String message) {
+        super(message);
+    }
+    
+}
